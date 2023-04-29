a604b7a3abf11c136cffacb712772f2133731928
arelimdz <arelimendozaperez@gmail.com>
2023-04-29 14:12:21 +1000

    Set main game loop

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ board = []
 for row_index in range(num_rows):
     board.append([None] * num_columns)
 
-#  Get input from player and verify if is valid
+#  Get input from player and verify if valid
 def get_valid_position():
     while True:
         try:
@@ -20,17 +20,22 @@ def get_valid_position():
         except ValueError:
             pass
 
-markers = ["X", "O"]
-position = get_valid_position()
+
+markers = ["X"]
+
+# Game Loop 
+while True:
+
+    
+    position = get_valid_position()
+    # Get board index
+    r = row_index = (position - 1) // num_columns
+    c = column_index = (position - 1) % num_columns
+
+    if board[r][c] is None:
+        board[r][c] = markers
+        pprint (board)
+    else:
+        print("Sorry spot taken. TRY AGAIN!")
 
 
-# Get board index
-r = row_index = (position - 1) // num_columns
-c = column_index = (position - 1) % num_columns
-
-if board[r][c] is None:
-    board[r][c] = markers
-else:
-    print("Sorry spot taken. TRY AGAIN!")
-
-pprint (board)
