a1e074cc935607f37d15cfaa5d2f23670265ee31
arelimdz <arelimendozaperez@gmail.com>
2023-04-29 12:53:53 +1000

    Define board position from user input

--- game.py
+++ game.py
@@ -9,7 +9,15 @@ for row_index in range(num_rows):
     board.append([None] * num_columns)
 
 #  Get input from player
-number = input("player input: ")
-if 1 <= int(number) <= (num_rows * num_columns):
-    return int(number)
+def get_valid_position():
+    number = input("player input: ")
+    if 1 <= int(number) <= (num_rows * num_columns):
+        return int(number)
 
+# Get board index
+position = get_valid_position()
+
+r = row_index = (position - 1) // num_columns
+c = column_index = (position - 1) % num_columns
+
+print(r, c)
