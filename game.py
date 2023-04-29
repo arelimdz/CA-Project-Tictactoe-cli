919ed8c68d822927bd6492eecd4e8af74a664c03
arelimdz <arelimendozaperez@gmail.com>
2023-04-29 11:59:42 +1000

    Prompt for user input

--- game.py
+++ game.py
@@ -8,3 +8,8 @@ board = []
 for row_index in range(num_rows):
     board.append([None] * num_columns)
 
+#  Get input from player
+number = input("player input: ")
+if 1 <= int(number) <= (num_rows * num_columns):
+    return int(number)
+
