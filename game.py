7debc2513a391bc2c3adeefee826ab925b2e4b72
arelimdz <arelimendozaperez@gmail.com>
2023-04-30 20:56:55 +1000

    Set win checker logic and link to game loop

--- game.py
+++ game.py
@@ -1,44 +1,125 @@
 from pprint import pprint
+from win_checker import is_winning_move
 
 # Define board size
 # TODO: Load from settings file
 num_rows = 3
 num_columns = 3
+target = 3
+board_size = num_columns * num_rows
 
 # Create board
 board = []
 for row_index in range(num_rows):
     board.append([None] * num_columns)
 
+
 #  Get input from player and verify if valid
-def get_valid_position():
+def get_valid_position(board):
     while True:
         try:
-            number = input("player input: ")
+            number = input("Please, enter your move: ")
+            position = int(number)
             if 1 <= int(number) <= (num_rows * num_columns):
-                return int(number)
+                r = row_index = (position - 1) // num_columns
+                c = column_index = (position - 1) % num_columns
+
+                if board[r][c] is not None:
+                    print("Oh Oh! Spot taken!")
+                    continue
+
+                return r, c
+            else:
+                print(f"Please, enter a number between 1 and {board_size}: ")
 
         except ValueError:
-            pass
+            if number.isalpha():
+                print(f"That is not a number! Please, enter only numbers: ")
 
 
 markers = ["X", "O"]
 current_marker_index = 0
+game_round = 1
+# Game Loop
 
-# Game Loop 
-while True:
-    
-    position = get_valid_position()
-    # Get board index
-    r = row_index = (position - 1) // num_columns
-    c = column_index = (position - 1) % num_columns
+horizontal_offsets = (0, -1), (0, 1)
+vertical_offsets = (-1, 0), (1, 0)
+forward_diagonal_offsets = (1, -1), (-1, 1)  # / diagonal
+backward_diagonal_offsets = (-1, -1), (1, 1)  # \ diagonal
 
-    if board[r][c] is None:
-        board[r][c] = markers[current_marker_index]
-        # Switch player
-        current_marker_index = (current_marker_index + 1) % len(markers)
-        pprint (board)
-    else:
-        print("Sorry spot taken. TRY AGAIN!")
+all_offsets = [
+    horizontal_offsets,
+    vertical_offsets,
+    forward_diagonal_offsets,
+    backward_diagonal_offsets,
+]
 
 
+def print_board(board):
+    board_position = 0
+    rows_to_print = []
+    for row_index, row in enumerate(board):
+        row_to_print = []
+        for column_index, column in enumerate(row):
+            board_position += 1
+            value = board[row_index][column_index]
+            if value is None:
+                row_to_print += str(board_position)
+            else:
+                row_to_print += value
+        rows_to_print.append("  " + "  |  ".join(row_to_print) + "\n")
+    print()
+    print("-----+-----+-----\n".join(rows_to_print))
+
+
+is_game_over = False
+while not is_game_over:
+    print_board(board)
+    r, c = get_valid_position(board)
+
+    current_marker = markers[current_marker_index]
+    # update board
+    board[r][c] = current_marker
+    # Switch player
+    move = (r, c)
+    print(move)
+
+    for offsets in all_offsets:
+        left_offset, right_offset = offsets
+        if is_winning_move(
+            board,
+            move,
+            current_marker,
+            target,
+            left_offset,
+            right_offset,
+        ):
+            print(f"WINNER {markers[current_marker_index]}")
+            print_board(board)
+            is_game_over = True
+            break
+
+    has_more_moves = False
+    for row in board:
+        for value in row:
+            if value is None:
+                has_more_moves = True
+                break
+
+    if not has_more_moves:
+        print("NO ONE WINS!")
+        break
+
+    current_marker_index = (current_marker_index + 1) % len(markers)
+
+
+# # result = is_winning_move(board, (2, 3), "O", 3, (-1, 0), (1, 0))  # horizontal
+# # result = is_winning_move(board, (1, 1), "X", 2, (-1, 0), (1, 0))  # vertical
+# result = is_winning_move(board, (2, 0), "X", 3, (1, -1), (-1, 1))  # / diagonal
+# # result = is_winning_move(board, (2, 2), "X", 3, (-1, -1), (1, 1))  # \ diagonal
+
+#     else:
+#         print("Sorry spot taken. TRY AGAIN!")
+#     if game_round > board_size:
+#         print("Game Over!")
+#         print("No winner")
--- win_checker.py
+++ win_checker.py
@@ -2,90 +2,79 @@ def is_marked(board, position, player_mark):
     return board[position[0]][position[1]] == player_mark
 
 
-def is_invalid_left_pointer(
-    left_pointer_row, left_pointer_column, left_row_limit, left_column_limit
-):
-    return left_pointer_row < left_row_limit or left_pointer_column < left_column_limit
-
-
-def is_invalid_right_pointer(
-    right_pointer_row, right_pointer_column, right_row_limit, right_column_limit
+def is_valid_pointer(
+    pointer_row,
+    pointer_column,
+    row_limit,
+    column_limit,
 ):
     return (
-        right_pointer_row > right_row_limit or right_pointer_column > right_column_limit
+        pointer_row >= 0
+        and pointer_row <= row_limit
+        and pointer_column >= 0
+        and pointer_column <= column_limit
     )
 
 
 def is_winning_move(board, move, mark, target, pointer_1_offset, pointer_2_offset):
-    pointer1 = list(move)
-    pointer2 = list(move)
-
     turns = ["pointer1", "pointer2"]
-    pointers = [
-        (pointer1, is_invalid_left_pointer, pointer_1_offset, (0, 0)),
-        (
-            pointer2,
-            is_invalid_right_pointer,
-            pointer_2_offset,
-            (len(board), len(board[0])),
-        ),
+    row_limit = len(board) - 1
+    column_limit = len(board[0]) - 1
+    pointer_configs = [
+        [list(move), pointer_1_offset],
+        [list(move), pointer_2_offset],
     ]
     current_turn_index = 0
     counter = 1
 
     pointer = True
-    while not ((pointer1 is None and pointer2 is None) or (counter == target)):
+    while not (
+        all(pointer_config[0] is None for pointer_config in pointer_configs)
+        or (counter == target)
+    ):
         (
             pointer,
-            is_invalid_pointer,
             pointer_offset,
-            limits,
-        ) = pointers[current_turn_index]
-        # Change the pointer position to 1 left
+        ) = pointer_configs[current_turn_index]
+
         if pointer is not None:
             pointer_row, pointer_column = pointer
             pointer_offset_row, pointer_offset_column = pointer_offset
             pointer_row += pointer_offset_row
             pointer_column += pointer_offset_column
-            row_limit, column_limit = limits
 
-            is_valid_pointer_result = not is_invalid_pointer(
+            is_valid_pointer_result = is_valid_pointer(
                 pointer_row, pointer_column, row_limit, column_limit
             )
 
             if is_valid_pointer_result and is_marked(
                 board, (pointer_row, pointer_column), mark
             ):
-                print(
-                    pointer_row,
-                    pointer_column,
-                    board[pointer_row][pointer_column],
-                    mark,
-                )
                 counter += 1
-                print("counter", counter)
-            else:
-                is_valid_pointer_result = False
+                if counter == target:
+                    return True
 
-            is_invalid_pointer_result = not is_valid_pointer_result
-            if is_invalid_pointer_result:
-                print(pointer_row, pointer_column, "is now None")
-                pointer = None
-                current_turn_index = (current_turn_index + 1) % len(turns)
-                pointer = pointers[current_turn_index][0]
+                pointer_configs[current_turn_index][0] = (pointer_row, pointer_column)
+                continue
+
+            pointer_configs[current_turn_index][0] = None
+            current_turn_index = (current_turn_index + 1) % len(turns)
+            pointer = pointer_configs[current_turn_index][0]
 
     return counter == target
 
 
-board = [["X", "O", "X"], [None, "X", "X"], [None, "X", None]]
-# board = [["X", None, "O", "X"], [None, None, "X", "X"], [None, None, None, None]]
-move = (1, 1)
+# board = [["X", None, "O"], [None, "X", None], [None, None, None]]
+# # board = [["X", None, "O", "X"], [None, None, "X", "X"], [None, None, None, None]]
+# # move = (1, 1)
 
 
-num_rows = len(board)
-num_columns = len(board[0])
+# num_rows = len(board)
+# num_columns = len(board[0])
 
-# result = is_winning_move(board, move, "X", 3, (0, -1), (0, 1))  # horizontal
-result = is_winning_move(board, move, "X", 3, (-1, 0), (1, 0))  # vertical
+# # result = is_winning_move(board, (2, 3), "O", 3, (-1, 0), (1, 0))  # horizontal
+# # result = is_winning_move(board, (1, 1), "X", 2, (-1, 0), (1, 0))  # vertical
+# result = is_winning_move(board, (2, 0), "X", 3, (1, -1), (-1, 1))  # / diagonal
+# # result = is_winning_move(board, (2, 2), "X", 3, (-1, -1), (1, 1))  # \ diagonal
 
-print(result)
+# print(result)
