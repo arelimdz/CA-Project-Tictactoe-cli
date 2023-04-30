11f6b20992472a81e5fdadc81306a5e704e39b95
arelimdz <arelimendozaperez@gmail.com>
2023-04-30 13:03:22 +1000

    Build win checker - set pointer1 loop

--- win_checker.py
+++ win_checker.py
@@ -7,8 +7,8 @@ num_columns = 3
 board_limits_per_row = num_rows -1
 mark = "X"
 
-
-pointer1 = list(move)  # [0,1]--> None
+pointer1 = [0, None]
+# pointer1 = list(move)  # [0,1]--> None
 pointer2 = list(move)   # [0,1]--> [0,2] ---> None
 def update_board(pointer1):
     position = board[pointer1[0]][pointer1[1]]
@@ -26,16 +26,30 @@ def is_mark():
 
 
 if turn == "pointer1":
-    pointer1[1] = pointer1[1] -1
-    print(update_board(pointer1))
-    #Check if is limits
-    if pointer1[1] < 0:
-        pointer1[1] = None
+    # Check if pointer 1 is mark None, stop the loop
+    if pointer1[1] == None:
+        print("Aqui termina")
+    # Change the pointer position to 1 left
     else: 
-        if is_mark() is True:
-            print(pointer1)
+        pointer1[1] = pointer1[1] -1
+
+        #Check if pointer is inside of board limits
+        if pointer1[1] < 0:
+            pointer1[1] = None
+
+        else: 
+            # Check is position is mark with "X"
+            if is_mark() is True:
+                # Confirm the new position for pointer1
+                pointer1
+
+            else: 
+                # If position is mark diferente or empty mark with None
+                pointer1[1] = None
+
 
 print (pointer1)
+
 
 
 
