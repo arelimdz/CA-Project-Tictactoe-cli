a9bd1de924462566d8e5f618f73c72c47ddd921d
arelimdz <arelimendozaperez@gmail.com>
2023-04-30 14:03:13 +1000

    Fix horizontal win checker logic

--- win_checker.py
+++ win_checker.py
@@ -1,44 +1,38 @@
-board = [["asnad", "X", None],[None,"X","X"], [None , None, None]]
+# board = [["X", "X", "X"],[None,"X","X"], [None , None, None]]
+board = [["X", "X", "X", "X"],[None, None,"X","X"], [None , None, None, None]]
 move = (0,1) 
 
 step = 1
-num_rows = 3
-num_columns = 3
-board_limits_per_row = num_rows -1
+num_rows = len(board)
+num_columns = len(board[0])
+print(num_rows, num_columns)
 mark = "X"
 
-pointer1 = [0, None]
-# pointer1 = list(move)  # [0,1]--> None
-pointer2 = list(move)   # [0,1]--> [0,2] ---> None
-def update_board(pointer1):
-    position = board[pointer1[0]][pointer1[1]]
-    return (position)
+pointer1 = list(move)
+pointer2 = list(move)
 
 
-turn = [pointer1, pointer2]
+turn = ["pointer1", "pointer2"]
 current_turn_index = 0
 counter = 1
-target = 3
+target = min(num_rows, num_columns)
 has_win = False
 
-# Check if is mark
-def is_mark(turn):
-    if update_board(turn) == mark:
-        return True
+
+def is_marked(position, player_mark):
+    return board[position[0]][position[1]] == player_mark
 
 
 
+while not ((pointer1[1] is None and pointer2[1] is None) or (counter == target)):
 
-
-while not ((pointer1 is None and pointer2 is None) or (counter == target)):
-
-    if turn == pointer1:
+    if turn[current_turn_index] == "pointer1":
         # Check if pointer 1 is mark None, stop the loop
-        if pointer1[1] == None:
-            print("Aqui termina")
+        if pointer1[1] is None:
+            print("Break here")
         # Change the pointer position to 1 left
         else: 
-            pointer1[1] = pointer1[1] -1
+            pointer1[1] = pointer1[1] - 1
 
             #Check if pointer is inside of board limits
             if pointer1[1] < 0:
@@ -46,37 +40,40 @@ while not ((pointer1 is None and pointer2 is None) or (counter == target)):
 
             else: 
                 # Check is position is mark with "X"
-                if is_mark(turn) is True:
+                if is_marked(pointer1, "X"):
                     # Confirm the new position for pointer1
-                    pointer1
+                    counter += 1
 
                 else: 
                     # If position is mark diferente or empty mark with None
                     pointer1[1] = None
+    
 
-    elif turn == pointer2:
+    elif turn[current_turn_index] == "pointer2":
 
         # Check if pointer 2 is mark None, stop the loop
-        if pointer2[1] == None:
-            print("Aqui termina")
+        if pointer2[1] is None:
+            print("Break Here")
         # Change the pointer position to 1 rigth
         else: 
-            pointer2[1] = pointer2[1] -1
+            pointer2[1] = pointer2[1] + 1
 
             #Check if pointer is inside of board limits
-            if pointer2[1] > board_limits_per_row:
+            if pointer2[1] > num_columns - 1:
                 pointer2[1] = None
 
             else: 
                 # Check is position is mark with "X"
-                if is_mark(turn) is True:
+                if is_marked(pointer2, "X"):
                     # Confirm the new position for pointer2
-                    pointer2
+                    counter += 1
 
                 else: 
                     # If position is mark diferente or empty mark with None
                     pointer2[1] = None
 
-
+    current_turn_index = (current_turn_index + 1) % len(turn)
+    print (counter)
+    print(pointer1, pointer2)
 
 
