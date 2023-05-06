285ca84c3d21a2d8127947d164bebdb6b815e258
arelimdz <arelimendozaperez@gmail.com>
2023-05-06 22:08:10 +1000

    Handle keyboard interrupt exception gracefully

--- src/main.py
+++ src/main.py
@@ -2,7 +2,10 @@ from menu_system import start_menu_system
 
 
 def main():
-    start_menu_system()
+    try:
+        start_menu_system()
+    except KeyboardInterrupt:
+        return
 
 
 if __name__ == "__main__":
